6463b5a8024faf5c7675410bd477852fc2f90e85
Vlad <vlad.khvesyukovich@gmail.com>
2022-11-11 19:22:37 +0300

    c1

---
+++ Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+var tcpListener = new TcpListener(localAddr, 8888);
+
+string path = @"C:\Users\eugene\Documents\content.txt";
+
+try
+{
+
+}
+catch (Exception e)
+{
+
+}
+finally 
+{
+
+}
